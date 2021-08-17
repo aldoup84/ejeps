98448709418f0076a61e1537b8585e0e9ec2bce0
aldoup84 <aldo.up@culiacan.tecnm.mx>
2021-08-17 17:27:35 +0000

    Primer ejercicio con nombres

---
+++ assignments/00Nombre/src/exercise.py
@@ -0,0 +1,9 @@
+def main():
+    nombre = input("Escribe tu nombre: ")
+    apPat = input("Escribe tu apellido: ")
+    apMat = input("Escribe tu apellido: ")
+    nc = nombre + " " + apPat + " " + apMat
+    print("Es un gusto conocerte " + nc)
+
+if __name__=='__main__':
+    main()
